6120ea62de516471ebbfb691cb19238811ecab3a
SanamSC <20schandi.acc@lwetb.ie>
2025-01-12 20:26:25 +0000

    Add files via upload

--- algorith and flowcharts coded.py
+++ algorith and flowcharts coded.py
@@ -1,4 +1,5 @@
-#1 calculate roots:
+"""
+#1 - calculate roots:
 A = int(input("enter a value for A"))
 B = int(input("enter a value for B"))
 C=  int(input("enter a value of B"))
@@ -10,7 +11,7 @@ root2=-B-(B**2 - 4*A*C)**0.5
 root2 = root2/divide
 print(root2)
 
-#2 read 3 numbers
+#2 - read 3 numbers
 num1 = int(input("enter 1st number"))
 num2 = int(input("enter 2nd number"))
 num3 = int(input("enter 3rd number"))
@@ -20,7 +21,36 @@ elif num2>num1 and num2>num3:
     print(num2,"is greatest number")
 else:
     print(num3, "is greatest number")
-    
-#3print all num 1-50
+
+#3 - print all num 1-50
 num = 50
-for i in range
+for i in range(50):
+    if i%2 ==0:
+        print(i)
+
+#4 - find sum of series 1+2+3...N, n is input
+N= int(input("enter a valur for N"))
+total = 0
+for a in range(1, N+1):
+    total +=a
+print("sum of series is", total)
+
+#5 - find sum and average of series of inputed numbers, stop if input is -1
+sum = 0
+count = 0
+numba = int(input("enter a number"))
+while numba != -1:
+    sum = sum + numba
+    count = count+1
+    numba = int(input("enter a number"))
+    if numba == -1:
+        average = sum/count
+print(average)
+print(sum)
+"""
+#6 - find factorial of N, n is inputed
+N2 = int(input("enter a number"))
+total = 1
+for a in range(1, N2+1):
+    total *=a
+print("factorial of N2 is", total)
